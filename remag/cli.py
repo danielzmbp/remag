ed671aa3e2cc37fec4f81edb4489a8db684bebcf
danielzmbp <39409778+danielzmbp@users.noreply.github.com>
2026-01-18 09:08:11 +0000

    tune: lower contamination penalty

--- remag/cli.py
+++ remag/cli.py
@@ -357,7 +357,7 @@ def validate_coverage_options(ctx, param, value):
     type=float,
     default=-10.0,
     show_default=True,
-    help="Minimum quality score (SCG - 10*Dups) required to accept a bin in greedy clustering.",
+    help="Minimum quality score (SCG - 5*Dups) required to accept a bin in greedy clustering.",
 )
 @click.option(
     "--leiden-k-neighbors",
