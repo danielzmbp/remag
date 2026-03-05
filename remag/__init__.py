def25ee87500a9b6e6f7808c6c31168a81e486ee
danielzmbp <daniel.gomez-perez@earlham.ac.uk>
2026-03-05 21:50:35 +0000

    release: bump version to 0.4.0

--- remag/__init__.py
+++ remag/__init__.py
@@ -6,7 +6,7 @@ try:
     from ._version import __version__
 except ImportError:
     # Fallback for Bioconda/conda installations without git
-    __version__ = "0.3.4"
+    __version__ = "0.4.0"
 
 __author__ = "Daniel Gómez-Pérez"
 __email__ = "daniel.gomez-perez@earlham.ac.uk"
